ea853a156754645567efabb391ffab0087c30eb9
Pankaj Sharma <me.pankaj786@gmail.com>
2023-03-30 18:28:20 +0530

    Function, closures and FizzBuzz

---
+++ 05_functions_closures/README.md
@@ -0,0 +1,7 @@
+#### Functions and closures
+- functions are declaredd with a name using the fn keyword in Rust
+  * can be nested
+  * nested functions are what python calls closures 
+- In Rust, closures are anonymous function-like construct you can store in a variable
+  * their practical use is mostly with iterators for e.g map, filter, reduce
+  * python has lambda functions but they are limited to one statement on a single line
---
+++ 05_functions_closures/rust/closures.rs
@@ -0,0 +1,28 @@
+fn main() {
+    let example_closure = |x| x;
+
+    let s = example_closure(String::from("hello"));
+    let n = example_closure("5".to_string()); // "5" is &str
+    // example_closure argument type is String. That is decided when we first called closure.
+    // So it will now only take string type argument.
+
+    println!("{} {}", s, n);
+
+    let x = vec![1, 2, 3];
+
+    let equal_to_x = move |z| z == x;
+
+    // ERROR: x has been moved into closure.
+    // No more it exist in main() scope
+    // println!("this is x {:?}", x);
+    let y = vec![1, 2, 3];
+    assert!(equal_to_x(y));
+
+    let items: Vec<i32> = vec![1, 2, 3, 4, 5];
+    let plus_one: Vec<_> = items.iter().map(|x| x + 1).collect();
+    let sum_all: i32 = items.iter().map(|x| x + 1).sum();
+    println!("{:?} {}", plus_one, sum_all);
+
+    let two_args = |x, y| x - y;
+    println!("{}", two_args(5, 3));
+}
